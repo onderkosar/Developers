e9fd9939538c8933986030290ac13502221a147c
Önder Koşar <onderkosar@hotmail.com>
2020-10-13 22:49:21 +0300

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Önder Koşar on 13.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
