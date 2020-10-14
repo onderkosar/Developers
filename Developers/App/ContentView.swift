6054f257f149514233ae4e12ecad121930ed21e5
Önder Koşar <onderkosar@hotmail.com>
2020-10-14 15:32:59 +0300

    scroll view is added for cards, data&model files are created

--- Developers/App/ContentView.swift
+++ Developers/App/ContentView.swift
@@ -9,17 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
-    
+    var cards: [Card] = cardData
     
     // MARK: - BODY
     var body: some View {
-        CardView()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(cards) { item in
+                    CardView(card: item)
+                }
+            } //: HSTACK
+            .padding(20)
+        } //: SCROLL VİEW
     }
 }
 
 // MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(cards: cardData)
     }
 }
